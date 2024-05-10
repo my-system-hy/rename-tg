4288ce2db39cf441c0fd03abeba8ffa12fb74f28
my-system-hy <gxxhbuc@gmail.com>
2024-05-11 00:34:33 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -6,8 +6,8 @@ class Database:
 
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-        self.roheshbots = self._client[database_name]
-        self.col = self.roheshbots.user
+        self.sexibot = self._client[database_name]  # Renamed to 'sexibot'
+        self.col = self.sexibot.user
 
     def new_user(self, id):
         return dict(
@@ -69,8 +69,4 @@ class Database:
         return user.get('suffix', None)              
 
 
-roheshbots = Database(Config.DB_URL, Config.DB_NAME)
-
-
-
-
+sexibot = Database(Config.DB_URL, Config.DB_NAME)  # Renamed the instance variable to 'sexibot'
