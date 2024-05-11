65cd9183d0f078fd6ffbc5a391a0240376c09ad6
my-system-hy <gxxhbuc@gmail.com>
2024-05-11 08:17:19 +0530

    Update thumbnail.py

--- plugins/thumbnail.py
+++ plugins/thumbnail.py
@@ -1,10 +1,10 @@
 from pyrogram import Client, filters 
-from helper.database import roheshbots
+from helper.database import seximodbots
 
 
 @Client.on_message(filters.private & filters.command(['view_thumb', 'viewthumb']))
 async def viewthumb(client, message):    
-    thumb = await roheshbots.get_thumbnail(message.from_user.id)
+    thumb = await seximodbots.get_thumbnail(message.from_user.id)
     if thumb:
        await client.send_photo(chat_id=message.chat.id, photo=thumb)
     else:
@@ -12,11 +12,11 @@ async def viewthumb(client, message):
 		
 @Client.on_message(filters.private & filters.command(['del_thumb', 'delthumb']))
 async def removethumb(client, message):
-    await roheshbots.set_thumbnail(message.from_user.id, file_id=None)
+    await seximodbots.set_thumbnail(message.from_user.id, file_id=None)
     await message.reply_text("❌️ __**Tʜᴜᴍʙɴᴀɪʟ Dᴇʟᴇᴛᴇᴅ**__")
 	
 @Client.on_message(filters.private & filters.photo)
 async def addthumbs(client, message):
     mkn = await message.reply_text("Please Wait ...")
-    await roheshbots.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
+    await seximodbots.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
     await mkn.edit("✅️ __**Tʜᴜᴍʙɴᴀɪʟ Sᴀᴠᴇᴅ**__")
